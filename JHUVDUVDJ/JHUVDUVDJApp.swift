13d37253171892642dc57e82bf72aaedbf465d74
Taina Saenz <tainamsaenz@gmail.com>
2023-07-31 16:24:10 -0700

    Initial Commit

---
+++ JHUVDUVDJ/JHUVDUVDJApp.swift
@@ -0,0 +1,17 @@
+//
+//  JHUVDUVDJApp.swift
+//  JHUVDUVDJ
+//
+//  Created by Taina Saenz on 7/31/23.
+//
+
+import SwiftUI
+
+@main
+struct JHUVDUVDJApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
